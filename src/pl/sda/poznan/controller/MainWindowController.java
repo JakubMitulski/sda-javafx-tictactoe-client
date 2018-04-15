988b31ff8dd007cc6b9912c0b690ffe9c04251e0
Jakub <jakub.mitulski@gmail.com>
2018-04-15 12:11:49 +0200

    Added connection with server.

--- src/pl/sda/poznan/controller/MainWindowController.java
+++ src/pl/sda/poznan/controller/MainWindowController.java
@@ -2,22 +2,24 @@ package pl.sda.poznan.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Label;
 import pl.sda.poznan.viewmodel.ConnectionDialogViewModel;
+import sun.rmi.runtime.Log;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static java.util.logging.Level.INFO;
-
 
 //Executing when user takes NewGame
-public class Controller {
+public class MainWindowController {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
 
@@ -27,7 +29,7 @@ public class Controller {
         System.out.println(source.getId());
     }
 
-    public void connectToServer(ActionEvent actionEvent) {
+    public void connectToServerAction(ActionEvent actionEvent) {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("ConnectionDialogWindow.fxml"));
 
@@ -57,5 +59,20 @@ public class Controller {
         logger.log(Level.INFO, String.format("Trying to connect server at adress {} with username {}",
                 viewModel.getServerAdress(), viewModel.getPlayerName()));
 
+        String[] adress = viewModel.getServerAdress().split(":");
+        String host = adress[0];
+        int port = Integer.parseInt(adress[1]);
+        // todo: add server validation - display error dialog if sth is wrong
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(host, port));
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Cannot connect to server: " + e.getMessage());
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Something went wrong...");
+            alert.setContentText("Could not connect to server");
+            alert.showAndWait();
+        }
     }
 }
